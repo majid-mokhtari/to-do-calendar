8a97b7397d095cbaa2a573858bfd5580d55160db
Majid Mokhtari <majidmo64@gmail.com>
2016-06-25 20:17:37 -0500

    updates added like editing and deleting data

--- js/services/data.js
+++ js/services/data.js
@@ -12,30 +12,98 @@ angular.module("todoListApp")
 	 //    	.then(callBack);
 	 //    };
 
-    this.getTodos = function(){
+  this.getTodos = function(){
     	return self.data;
  	}
 
-	this.deleteTodo = function(todo){
-		console.log("deleted " + todo.name);
-	}
+  //edit todo/event from google calender
+  this.editEvent = function(todo){
 
-	this.saveTodo = function(todo){
-		console.log("saved " + todo.name);
-	}
+    var request = gapi.client.calendar.events.get({
+      'calendarId': 'primary',
+      'eventId': todo.id
+    });
 
+    request.execute(function(resp){
+      var event = resp;
+      event.summary = todo.name;
+      var request = gapi.client.calendar.events.update({
+        'calendarId': 'primary',
+        'resource': event,
+        'eventId': todo.id
+      });
+
+      request.execute(function(event) {
+        appendPre('Event created: ' + event.htmlLink);
+        listUpcomingEvents()
+      });
+    });
+  }
+
+  //delete todo/event from google calender
+  this.deleteEvent = function(params){
+
+    var request = gapi.client.calendar.events.delete({
+      'calendarId': 'primary',
+      'eventId': params.id
+    });
+
+    request.execute(function(event) {
+      appendPre('Event created: ' + event.htmlLink);
+    });
+  }
+
+
+  //insert event into google calender
+  this.insertEvent = function(todo) {
+    var event = {
+      'summary': todo.name,
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+          'dateTime': todo.startDate,
+          'timeZone': 'America/Chicago'
+        },
+        'end': {
+          'dateTime': todo.endDate,
+          'timeZone': 'America/Chicago'
+        },
+        'attendees': [
+          {'email': 'lpage@example.com'},
+          {'email': 'sbrin@example.com'}
+        ],
+        'reminders': {
+          'useDefault': false,
+          'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10}
+          ]
+        }
+    }
+
+    var request = gapi.client.calendar.events.insert({
+      'calendarId': 'primary',
+      'resource': event
+    });
+
+    request.execute(function(event) {
+      appendPre('Event created: ' + event.htmlLink);
+      listUpcomingEvents()
+    });
+  }
+  
 /*
 ==============================================
 Google Calender API
 ==============================================
 */
       // local
-      var CLIENT_ID = 'Your_ClietnID';
+      var CLIENT_ID = '757261987194-pb9bah2jgvkbrqncs2p4htls222cq1c9.apps.googleusercontent.com';
 
       // prod
-      //var CLIENT_ID = 'Your_ClietnID'
+      //var CLIENT_ID = '757261987194-lvfdj8e5qv9o2i7htrgmm8pqp5n71s3u.apps.googleusercontent.com'
 
-      var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+      var SCOPES = ["https://www.googleapis.com/auth/calendar"];
 
       /**
        * Check if current user has authorized this application.
@@ -57,7 +125,7 @@ Google Calender API
       function handleAuthResult(authResult) {
         if (authResult && !authResult.error) {
           // Hide auth UI, then load client library.
-          loadCalendarApi();
+            loadCalendarApi();
         } else {
           // Show auth UI, allowing the user to initiate authorization by
           // clicking authorize button.
@@ -70,7 +138,7 @@ Google Calender API
        *
        * @param {Event} event Button click event.
        */
-      this.handleAuthClick = function(event) {
+      this.handleAuthClick = function() {
         gapi.auth.authorize(
           {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
           handleAuthResult);
@@ -82,7 +150,7 @@ Google Calender API
        * once client library is loaded.
        */
       function loadCalendarApi() {
-        gapi.client.load('calendar', 'v3', listUpcomingEvents);
+          gapi.client.load('calendar', 'v3', listUpcomingEvents);
       }
 
       /**
@@ -102,18 +170,16 @@ Google Calender API
 
         request.execute(function(resp) {
           var events = resp.items;
+              self.data = [];
 
           if (events.length > 0) {
-            for (var i = 0; i < events.length; i++) {
-
-        		var item = {};
-        		item.name = events[i].summary;
+              for (var i = 0; i < events.length; i++) {
+          		var item = {};
+          		item.name = events[i].summary;
+          		item.startDate = events[i].start.dateTime;
+              item.endDate = events[i].end.dateTime;
+              item.id = events[i].id;
             	self.data.push(item);
-              var event = events[i];
-              var when = event.start.dateTime;
-              if (!when) {
-                when = event.start.date;
-              }
             }
           } else {
             appendPre('No upcoming events found.');
@@ -122,12 +188,6 @@ Google Calender API
         });
       }
 
-      /**
-       * Append a pre element to the body containing the given message
-       * as its text node.
-       *
-       * @param {string} message Text to be placed in pre element.
-       */
       function appendPre(message) {
         // when there is no to do items
       }
